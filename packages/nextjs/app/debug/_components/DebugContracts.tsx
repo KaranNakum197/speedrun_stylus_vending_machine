2e0f67f25f84612ec0a0e995c290f044a8a20c65
KaranNakum197 <karannakum456@gmail.com>
2025-08-15 09:53:52 +0000

    Initial commit for challenge submission

--- packages/nextjs/app/debug/_components/DebugContracts.tsx
+++ packages/nextjs/app/debug/_components/DebugContracts.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { IVendingMachine } from "./IVendingMachine";
 import { ethers } from "ethers";
 
-const contractAddress = "0x4f6e1bc44ee6f1dd0951b8e5bde4764055918055"; // Get this from run-dev-node.sh output
+const contractAddress = "0xc9144e582ad1b871404be9a2e3018ea2697f1dd6"; // Get this from run-dev-node.sh output
 const provider = new ethers.JsonRpcProvider(process.env.NEXT_PUBLIC_RPC_URL || "");
 const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || "";
 const signer = new ethers.Wallet(privateKey, provider);
